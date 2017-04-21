cc5651539bb539cf067d2b3d50c1bd9a91df2e4a
jquesadaabeijon <jquesadaabeijon@victoria32.danielcastelao.org>
2017-04-21 12:30:28 +0200

    0.1

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author jquesadaabeijon
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Ventana1 ven1 = new Ventana1();
+//        ven1.setVisible(true);
+//        Ventana2 ven2 = new Ventana2();
+//        ven2.setVisible(true);
+//        Ventana3 ven3 = new Ventana3();
+//        ven3.setVisible(true);
+        Ventana4 ven4 = new Ventana4();
+        ven4.setVisible(true);
+    }
+    
+}
